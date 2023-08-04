6f99c326b4104a653b2801d39bc5d42fb9604f27
Jacob Pudwill <jacobpudwill@gmail.com>
2023-08-03 22:23:22 -0500

    Added get and post routes for /api/notes.

--- server.js
+++ server.js
@@ -14,9 +14,9 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-// app.get('*', (req, res) =>
-//   res.sendFile(path.join(__dirname, '/public/index.html'))
-// );
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
 app.get('/api/notes', (req, res) => {
   fs.readFile('./db/db.json', (err, data) => {
